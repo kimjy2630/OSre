e5c82ec973478b9d77d4293b04aa525c65da3eb9
Jaeyoon Kim <Jaeyoon@143.248.180.79>
2016-10-01 20:20:38 +0900

    Ver.0.1.16 add highest_priority declaration

--- pintos/src/threads/thread.h
+++ pintos/src/threads/thread.h
@@ -146,6 +146,9 @@ void thread_set_nice (int);
 int thread_get_recent_cpu (void);
 int thread_get_load_avg (void);
 
+//
+bool highest_priority(const struct list_elem *a, const struct list_elem *b, void *aux);
+//
 ////
 bool higher_priority(const struct list_elem *a, const struct list_elem *b, void *aux);
 
