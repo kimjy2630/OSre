a336af0895a3b60125488b3422ee1f324f9a8b1d
Jaeyoon Kim <Jaeyoon@143.248.180.79>
2016-10-01 13:18:01 +0900

    Ver.0.1.1 add list_lock to thread structure

--- pintos/src/threads/thread.h
+++ pintos/src/threads/thread.h
@@ -91,7 +91,9 @@ struct thread
 
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
+    //
     struct list list_lock;
+    //
 
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
