c51db8a557fca206f7c067623a173e11ee2e5167
Jaeyoon Kim <Jaeyoon@143.248.180.79>
2016-10-01 13:06:46 +0900

    made pintos folder

--- pintos/src/threads/thread.h
+++ pintos/src/threads/thread.h
@@ -91,6 +91,7 @@ struct thread
 
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
+    struct list list_lock;
 
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
