62e099c963d7c93ebdf06a68a4ccb9e1380ecc60
Jaeyoon Kim <Jaeyoon@143.248.180.79>
2016-10-01 21:02:07 +0900

    dbg

--- pintos/src/threads/thread.c
+++ pintos/src/threads/thread.c
@@ -582,8 +582,8 @@ next_thread_to_run (void)
   if (list_empty (&ready_list))
     return idle_thread;
   else
-	  return list_entry(list_max(&ready_list, highest_priority, NULL), struct thread, elem);
-//    return list_entry (list_pop_front (&ready_list), struct thread, elem);
+//	  return list_entry(list_max(&ready_list, highest_priority, NULL), struct thread, elem);
+    return list_entry (list_pop_front (&ready_list), struct thread, elem);
 }
 
 /* Completes a thread switch by activating the new thread's page
