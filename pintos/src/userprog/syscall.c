9171bd4cacdef75cf0494d264a9018604b259b07
Jaeyoon Kim <Jaeyoon@143.248.180.79>
2016-10-15 04:41:05 +0900

    remove comment

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -84,72 +84,39 @@ syscall_handler (struct intr_frame *f UNUSED)
 		break;
 	case SYS_EXIT:
 		// int type arg
-//		if (!read_validity(ptr + 1, 4)) {
-//			printf("invalid user pointer read\n");
-//			thread_exit();
-//		}
-//		exit(*((int *) ptr + 1));
 		exit(get_argument_int(ptr, 1));
 		break;
 	case SYS_EXEC:
 		// char* type arg
-//		if (!read_validity(ptr + 1, 4)) {
-//			printf("invalid user pointer read\n");
-//			thread_exit();
-//		}
-//		exec(*((char **) ptr + 1));
 		f->eax = exec(get_argument_ptr(ptr, 1));
 		break;
 	case SYS_WAIT:
 		// pid_t type arg
-//		if (!read_validity(ptr + 1, 4)) {
-//			printf("invalid user pointer read\n");
-//			thread_exit();
-//		}
-//		wait(*((pid_t *) ptr + 1));
 		f->eax = wait(get_argument_int(ptr, 1));
 		break;
 	case SYS_CREATE:
 		// char*, unsigned type arg
-//		if (!read_validity(ptr + 1, 8)) {
-//			printf("invalid user pointer read\n");
-//			thread_exit();
-//		}
-//		create(*((char **) ptr + 1), *(unsigned *) ptr + 2);
 		f->eax = create(get_argument_ptr(ptr, 1), get_argument_int(ptr, 2));
 		break;
 	case SYS_REMOVE:
 		// char* type arg
-//		if (!read_validity(ptr + 1, 4)) {
-//			printf("invalid user pointer read\n");
-//			thread_exit();
-//		}
-//		remove(*((char **) ptr + 1));
 		f->eax = remove(get_argument_ptr(ptr, 1));
 		break;
 	case SYS_OPEN:
 		// char* type arg
-//		if (!read_validity(ptr + 1, 4)) {
-//			printf("invalid user pointer read\n");
-//			thread_exit();
-//		}
-//		open(*((char **) ptr + 1));
 		f->eax = open(get_argument_ptr(ptr, 1));
 		break;
 	case SYS_FILESIZE:
 		// int type arg
-//		filesize(0);
 		f->eax = filesize(get_argument_int(ptr, 1));
 		break;
 	case SYS_READ:
 		// int, void*, unsigned type arg
-//		read(0, NULL, 0);
 		f->eax = read(get_argument_int(ptr, 1), get_argument_ptr(ptr, 2),
 				get_argument_int(ptr, 3));
 		break;
 	case SYS_WRITE:
 		// int, void*, unsigned type arg
-//		write(0, NULL, 0);
 //		printf("SYSWRITE %d_%p_%s_%u\n", get_argument_int(ptr, 1),
 //				get_argument_ptr(ptr, 2), get_argument_ptr(ptr, 2),
 //				get_argument_int(ptr, 3));
@@ -159,17 +126,14 @@ syscall_handler (struct intr_frame *f UNUSED)
 		break;
 	case SYS_SEEK:
 		// int, unsigned type arg
-//		seek(0, 0);
 		seek(get_argument_int(ptr, 1), get_argument_int(ptr, 2));
 		break;
 	case SYS_TELL:
 		// unsigned type arg
-//		tell(0);
 		f->eax = tell(get_argument_int(ptr, 1));
 		break;
 	case SYS_CLOSE:
 		// unsigned type arg
-//		close(0);
 		close(get_argument_int(ptr, 1));
 		break;
 	}
