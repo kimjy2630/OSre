9ff42d0058d418fe9543eb466355d67d26a7d416
Jaeyoon Kim <Jaeyoon@143.248.180.79>
2016-10-14 20:57:39 +0900

    test

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -437,6 +437,7 @@ void push_argument (int argc, char *last, void **esp){
 	// word-align
 //	int align_size = (int)(*esp) % 4;
 	int align_size = (*((int*) esp)) % 4;
+	printf("ALIGN %d\n",align_size);
 	if(align_size > 0){
 		i = 0;
 		push_stack(esp, &i, align_size);
