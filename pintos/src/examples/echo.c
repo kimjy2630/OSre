5f39b6eb65abaea49f97e143320e4b56cb997b18
Jaeyoon Kim <Jaeyoon@143.248.180.79>
2016-10-14 23:15:37 +0900

    echo test

--- pintos/src/examples/echo.c
+++ pintos/src/examples/echo.c
@@ -5,9 +5,9 @@ int
 main (int argc, char **argv)
 {
   int i;
-
+printf("%d\n", argc);
   for (i = 0; i < argc; i++)
-    printf ("%s ", argv[i]);
+    printf ("%d %s ",i, argv[i]);
   printf ("\n");
 
   return EXIT_SUCCESS;
