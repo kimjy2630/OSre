b1bf91f09b38e5051b6173f0a161b7467e5082b5
Jaeyoon Kim <Jaeyoon@143.248.180.79>
2016-10-14 22:47:52 +0900

    adsf

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -155,6 +155,7 @@ syscall_handler (struct intr_frame *f UNUSED)
 				get_argument_int(ptr, 3));
 		f->eax = write(get_argument_int(ptr, 1), get_argument_ptr(ptr, 2),
 				get_argument_int(ptr, 3));
+		printf("SYSWRITE RET %d\n", f->eax);
 		break;
 	case SYS_SEEK:
 		// int, unsigned type arg
@@ -212,6 +213,7 @@ int read (int fd, void *buffer, unsigned length){
 int write (int fd, const void *buffer, unsigned length){
 	if (write_validity(buffer, length))
 		if (fd == 1) { // write to console
+			printf("WRITE FD 1\n");
 			putbuf(buffer, (size_t) length);
 			return length;
 		}
