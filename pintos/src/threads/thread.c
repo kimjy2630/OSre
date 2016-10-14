812d88c258c3f309f31fac5a2d714c8fbb992dc9
Jaeyoon Kim <Jaeyoon@143.248.180.79>
2016-10-14 17:26:51 +0900

    asdf

--- pintos/src/threads/thread.c
+++ pintos/src/threads/thread.c
@@ -494,7 +494,7 @@ static void init_thread(struct thread *t, const char *name, int priority) {
 	#ifdef USERPROG
 	t->is_exit = false;
 	list_init(&t->list_children);
-	list_pushback(&thread_current()->list_children, &t->elem_child);
+	list_push_back(&thread_current()->list_children, &t->elem_child);
 //	lock_init(t->lock_child);
 //	lock_acquire(t->lock_child);
 	#endif
