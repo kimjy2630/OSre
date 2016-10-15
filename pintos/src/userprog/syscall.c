708df4342f22785acc5fb036efbeacdf0152e634
Jaeyoon Kim <Jaeyoon@143.248.180.79>
2016-10-15 13:44:06 +0900

    synatax

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -21,19 +21,19 @@ static bool write_validity(const void* udst, int size);
 static void* get_argument (void *ptr, int pos);
 
 
-static void halt (void);
-static void exit (int status);
-static pid_t exec (const char *file);
-static int wait (pid_t pid);
-static bool create (const char *file, unsigned initial_size);
-static bool remove (const char *file);
-static int open (const char *file);
-static int filesize (int fd);
-static int read (int fd, void *buffer, unsigned length);
-static int write (int fd, const void *buffer, unsigned length);
-static void seek (int fd, unsigned position);
-static unsigned tell (int fd);
-static void close (int fd);
+static void halt(void);
+static void exit(int status);
+static pid_t exec(const char *file);
+static int wait(pid_t pid);
+static bool create(const char *file, unsigned initial_size);
+static bool remove(const char *file);
+static int open(const char *file);
+static int filesize(int fd);
+static int read(int fd, void *buffer, unsigned length);
+static int write(int fd, const void *buffer, unsigned length);
+static void seek(int fd, unsigned position);
+static unsigned tell(int fd);
+static void close(int fd);
 ////
 
 void
