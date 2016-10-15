5d9af9e3b13b94497fcd46808d599ef122b147a5
하진철 <smilecjf@naver.com>
2016-10-16 03:25:06 +0900

    syscall header

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -12,27 +12,6 @@
 ////
 
 static void syscall_handler(struct intr_frame *);
-////
-static int get_user(const uint8_t *uaddr);
-static bool put_user(uint8_t *udst, uint8_t byte);
-static bool read_validity(const void *uaddr, int size);
-static bool write_validity(const void* udst, int size);
-static void* get_argument(void *ptr, int pos);
-
-static void halt(void);
-static void exit(int status);
-static pid_t exec(const char *file);
-static int wait(pid_t pid);
-static bool create(const char *file, unsigned initial_size);
-static bool remove(const char *file);
-static int open(const char *file);
-static int filesize(int fd);
-static int read(int fd, void *buffer, unsigned length);
-static int write(int fd, const void *buffer, unsigned length);
-static void seek(int fd, unsigned position);
-static unsigned tell(int fd);
-static void close(int fd);
-////
 
 void syscall_init(void) {
 	intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall");
