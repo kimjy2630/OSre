24d2d18944bf58d9c9b4c8c9682cd64941af17af
Jaeyoon Kim <Jaeyoon@143.248.180.79>
2016-10-17 22:35:19 +0900

    asdfa

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -211,7 +211,8 @@ void process_exit(void) {
 		curr->f = NULL;
 	}
 
-	if (curr->parent == NULL) {
+//	if (curr->parent == ) {
+	if(!curr->parent->user_thread)
 		if (curr->ps != NULL) {
 			//TODO
 			printf("free ps case 3 tid=%d name=%s\n", curr->parent->tid, curr->parent->name);
