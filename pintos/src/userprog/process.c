25bf17b21fff8367524d9295f8e1b2102c4f7ff6
Jaeyoon Kim <Jaeyoon@143.248.180.79>
2016-11-09 18:49:35 +0900

    ㅁㄴㅇㄹㅁㅇ

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -129,19 +129,11 @@ int process_wait(tid_t child_tid) {
 		}
 	}
 	if (flag) {
-		//TODO
-//		enum intr_level old_level = intr_disable();
-		while (child->t->user_thread && !child->t->is_exit) {
-			//TODO
-//			barrier();
-//			thread_block();
+		while (child->t->user_thread && !child->t->is_exit)
 			thread_yield();
-		}
-		//TODO
-//		intr_set_level(old_level);
 		int status = child->exit_status;
 		//TODO
-		if (child->t != NULL){
+		if (child->t != NULL) {
 			child->t->ps = NULL;
 			child->t->parent = NULL;
 		}
