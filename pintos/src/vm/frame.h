8b08b6d7cfa93185db48678e20b578aaddd9e2bd
Jaeyoon Kim <Jaeyoon@143.248.180.79>
2016-11-11 15:54:38 +0900

    frame.h

--- pintos/src/vm/frame.h
+++ pintos/src/vm/frame.h
@@ -9,7 +9,7 @@ struct frame_entry {
 void frame_init();
 //struct frame_entry* frame_get(uint8_t *addr);
 struct frame_entry* frame_add(enum palloc_flags flags);
-bool frame_free(uint8_t *addr);
+void frame_free(uint8_t *addr);
 
 
 
