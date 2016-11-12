e352fe3bb68906f48face0bf29889f7ed4598584
하진철 <smilecjf@naver.com>
2016-11-13 02:21:16 +0900

    asdf

--- pintos/src/vm/frame.c
+++ pintos/src/vm/frame.c
@@ -90,13 +90,13 @@ void frame_evict() {
 		intr_set_level(old_level);
 		printf("e:%p\n",e);
 		fe = list_entry(e, struct frame_entry, elem);
-//		printf("fe:%p\n",fe);
+		printf("fe:%p\n",fe);
 		pd = fe->t->pagedir;
-//		printf("pd:%p\n",pd);
+		printf("pd:%p\n",pd);
 		spe = fe->spe;
-//		printf("spe:%p\n",spe);
+		printf("spe:%p\n",spe);
 		uaddr = spe->uaddr;
-//		printf("uaddr:%p\n",uaddr);
+		printf("uaddr:%p\n",uaddr);
 		if(spe->type == SWAP){
 			printf("swap page\n");
 			frame_free(fe);
