c98597545cc18fd2332cda9b68771a0f432d1204
Jaeyoon Kim <Jaeyoon@143.248.180.79>
2016-11-12 13:13:41 +0900

    asdf

--- pintos/src/userprog/exception.c
+++ pintos/src/userprog/exception.c
@@ -191,7 +191,7 @@ page_fault (struct intr_frame *f)
 			 */
 
 			spe->kaddr = fe->addr;
-			if (!pagedir_set_page(t->pagedir, spe->uaddr, spe->kaddr,
+			if (!pagedir_set_page(t->pagedir, pg_round_down(fault_addr), spe->kaddr,
 					spe->writable)) {
 				kill(f);
 			}
