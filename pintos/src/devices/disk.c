5cb650f4c9311bd0afda1ab0a11b2be66d0abfb3
하진철 <smilecjf@naver.com>
2016-11-13 03:52:57 +0900

    asdf

--- pintos/src/devices/disk.c
+++ pintos/src/devices/disk.c
@@ -251,7 +251,6 @@ disk_write (struct disk *d, disk_sector_t sec_no, const void *buffer)
   ASSERT (buffer != NULL);
 
   c = d->channel;
-  printf("lock_acq  holder:%p, curr:%p\n", c->lock.holder, thread_current());
   lock_acquire (&c->lock);
   select_sector (d, sec_no);
   issue_pio_command (c, CMD_WRITE_SECTOR_RETRY);
