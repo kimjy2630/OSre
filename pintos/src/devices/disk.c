824742b2218120b0ba8d1ec0cc6084a1ac36da42
Jaeyoon Kim <Jaeyoon@143.248.180.79>
2016-11-13 04:49:48 +0900

    asdf

--- pintos/src/devices/disk.c
+++ pintos/src/devices/disk.c
@@ -250,6 +250,9 @@ disk_write (struct disk *d, disk_sector_t sec_no, const void *buffer)
   ASSERT (buffer != NULL);
 
   c = d->channel;
+	//TODO
+	printf("lock holder %p\n", c->lock.holder);
+	printf("current thread %p\n", thread_current());
   lock_acquire (&c->lock);
   select_sector (d, sec_no);
   issue_pio_command (c, CMD_WRITE_SECTOR_RETRY);
