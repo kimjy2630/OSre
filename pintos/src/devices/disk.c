085ce62a2e22fb62b4110b26bb4da31519fce2bf
하진철 <smilecjf@naver.com>
2016-11-13 06:44:00 +0900

    asfd

--- pintos/src/devices/disk.c
+++ pintos/src/devices/disk.c
@@ -227,14 +227,21 @@ disk_read (struct disk *d, disk_sector_t sec_no, void *buffer)
 
   c = d->channel;
   lock_acquire (&c->lock);
+  printf("disk_read 1");
   select_sector (d, sec_no);
+  printf("disk_read 2");
   issue_pio_command (c, CMD_READ_SECTOR_RETRY);
+  printf("disk_read 3");
   sema_down (&c->completion_wait);
+  printf("disk_read 4");
   if (!wait_while_busy (d))
     PANIC ("%s: disk read failed, sector=%"PRDSNu, d->name, sec_no);
   input_sector (c, buffer);
+  printf("disk_read 5");
   d->read_cnt++;
+  printf("disk_read 6");
   lock_release (&c->lock);
+  printf("disk_read 7");
 }
 
 /* Write sector SEC_NO to disk D from BUFFER, which must contain
