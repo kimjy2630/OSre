d49327ee60fa003beeea0b7a571cd46e86bdafa3
하진철 <smilecjf@naver.com>
2016-11-13 07:05:59 +0900

    asdf

--- pintos/src/userprog/exception.c
+++ pintos/src/userprog/exception.c
@@ -180,15 +180,15 @@ page_fault (struct intr_frame *f)
 			fe->spe = spe;
 
 //			printf("PASS\n");
-			spe->kaddr = fe->addr;
-			pagedir_clear_page(t->pagedir, pg_round_down(fault_addr));
-			if (!pagedir_set_page(t->pagedir, pg_round_down(fault_addr), spe->kaddr,
-							spe->writable)) {
-//				printf("KILL\n");
-				kill(f);
-			}
-			pagedir_set_dirty (t->pagedir, pg_round_down(fault_addr), false);
-			pagedir_set_accessed (t->pagedir, pg_round_down(fault_addr), true);
+//			spe->kaddr = fe->addr;
+//			pagedir_clear_page(t->pagedir, pg_round_down(fault_addr));
+//			if (!pagedir_set_page(t->pagedir, pg_round_down(fault_addr), spe->kaddr,
+//							spe->writable)) {
+////				printf("KILL\n");
+//				kill(f);
+//			}
+//			pagedir_set_dirty (t->pagedir, pg_round_down(fault_addr), false);
+//			pagedir_set_accessed (t->pagedir, pg_round_down(fault_addr), true);
 
 			if (spe->type == FILE) {
 //				printf("FILE\n");
@@ -211,16 +211,16 @@ page_fault (struct intr_frame *f)
 //				printf("swap sfad\n");
 			}
 
-////			printf("PASS\n");
-//			spe->kaddr = fe->addr;
-//			pagedir_clear_page(t->pagedir, pg_round_down(fault_addr));
-//			if (!pagedir_set_page(t->pagedir, pg_round_down(fault_addr), spe->kaddr,
-//					spe->writable)) {
-////				printf("KILL\n");
-//				kill(f);
-//			}
-//			pagedir_set_dirty (t->pagedir, pg_round_down(fault_addr), false);
-//			pagedir_set_accessed (t->pagedir, pg_round_down(fault_addr), true);
+//			printf("PASS\n");
+			spe->kaddr = fe->addr;
+			pagedir_clear_page(t->pagedir, pg_round_down(fault_addr));
+			if (!pagedir_set_page(t->pagedir, pg_round_down(fault_addr), spe->kaddr,
+					spe->writable)) {
+//				printf("KILL\n");
+				kill(f);
+			}
+			pagedir_set_dirty (t->pagedir, pg_round_down(fault_addr), false);
+			pagedir_set_accessed (t->pagedir, pg_round_down(fault_addr), true);
 //			printf("PAGE FAULT RETURN\n");
 			return;
 		} else {
