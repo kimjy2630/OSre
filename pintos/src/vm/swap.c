8c071166064b48739780814d9a2cb7918b196d00
하진철 <smilecjf@naver.com>
2016-11-13 00:30:19 +0900

    asdf

--- pintos/src/vm/swap.c
+++ pintos/src/vm/swap.c
@@ -2,7 +2,6 @@
 #include "threads/vaddr.h"
 #include "threads/synch.h"
 #include "lib/kernel/bitmap.h"
-#include "threads/interrupt.h"
 
 struct disk *swap_disk;
 struct bitmap *swap_bitmap;
@@ -20,7 +19,6 @@ void swap_init(){
 
 size_t swap_load(uint8_t *uaddr){ // mem -> disk
 //	lock_acquire(&swap_lock);
-	enum intr_level old_level = intr_disable();
 	size_t index = bitmap_scan_and_flip(swap_bitmap, 0, num_sector_in_page, 0);
 	if(index == BITMAP_ERROR)
 		PANIC("swap disk full");
@@ -29,26 +27,21 @@ size_t swap_load(uint8_t *uaddr){ // mem -> disk
 		disk_write(swap_disk, index + i, uaddr + i * DISK_SECTOR_SIZE);
 	}
 //	lock_release(&swap_lock);
-	intr_set_level(old_level);
 	return index;
 }
 
 void swap_unload(size_t index, uint8_t *uaddr){ // disk -> mem
 //	lock_acquire(&swap_lock);
-	enum intr_level old_level = intr_disable();
 	bitmap_set_multiple(swap_bitmap, index, num_sector_in_page, 0);
 	int i;
 	for(i=0; i<num_sector_in_page; i++){
 		disk_read(swap_disk, index + i, uaddr + i * DISK_SECTOR_SIZE);
 	}
-	intr_set_level(old_level);
 //	lock_release(&swap_lock);
 }
 
 void swap_free(size_t index){
 //	lock_acquire(&swap_lock);
-	enum intr_level old_level = intr_disable();
 	bitmap_set_multiple(swap_bitmap, index, num_sector_in_page, 0);
 //	lock_release(&swap_lock);
-	intr_set_level(old_level);
 }
