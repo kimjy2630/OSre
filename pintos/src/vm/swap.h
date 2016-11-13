014a643e75df168cf5a692d214de14e978b6af6d
하진철 <smilecjf@naver.com>
2016-11-13 19:56:39 +0900

    asdf

--- pintos/src/vm/swap.h
+++ pintos/src/vm/swap.h
@@ -4,7 +4,7 @@
 
 void swap_init();
 size_t swap_load(uint8_t *uaddr);
-void swap_unload(size_t index, uint8_t *uaddr);
+void swap_unload(size_t index, struct supp_page_entry *spe);
 void swap_free(size_t index);
 
 #endif
