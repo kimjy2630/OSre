4de8c18dbeac2c8b63e916029086e0ec3bde00a8
하진철 <smilecjf@naver.com>
2016-11-13 23:33:43 +0900

    fsda

--- pintos/src/vm/frame.h
+++ pintos/src/vm/frame.h
@@ -13,7 +13,7 @@ struct frame_entry {
 
 void frame_init();
 struct frame_entry* frame_lookup(uint8_t *kddr);
-struct frame_entry* frame_add(enum palloc_flags flags);
+//struct frame_entry* frame_add(enum palloc_flags flags);
 void frame_free(struct frame_entry *fe);
 
 
