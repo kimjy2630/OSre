7fcdaa5cc56bf8c7760085a9e14e506c12144883
하진철 <smilecjf@naver.com>
2016-11-13 21:37:53 +0900

    asdf

--- pintos/src/userprog/exception.c
+++ pintos/src/userprog/exception.c
@@ -256,11 +256,11 @@ static void page_fault(struct intr_frame *f) {
 				return;
 			}
 
-			printf("fault_addr:%p\n", fault_addr);
+//			printf("fault_addr:%p\n", fault_addr);
 			uint32_t offset = ((uint32_t *) PHYS_BASE) - ((uint32_t *)fault_addr);
 //			printf("offset:%p\n", offset);
 			if (offset > STACK_LIMIT) {
-				printf("stack overflow\n");
+//				printf("stack overflow\n");
 				f->eip = (void *) f->eax;
 				f->eax = 0xffffffff;
 				exit(-1);
@@ -268,7 +268,7 @@ static void page_fault(struct intr_frame *f) {
 			}
 
 			if ((fault_addr == esp - 4) || (fault_addr == esp - 32) || fault_addr >= esp) {
-				printf("333\n");
+//				printf("333\n");
 				/* Check for stack overflow */
 //				if (fault_addr < STACK_MIN) {
 //					exit(-1);
