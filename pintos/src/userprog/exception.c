f023fbcbb452d6efdf2c03f73a34862f66fa6099
하진철 <smilecjf@naver.com>
2016-11-14 21:03:28 +0900

    sadf

--- pintos/src/userprog/exception.c
+++ pintos/src/userprog/exception.c
@@ -298,10 +298,10 @@ static void page_fault(struct intr_frame *f) {
 			}
 		}
 	} else {
-//		printf("present\n");
-//		printf("write %d\n", write);
-//		printf("user %d\n", user);
-//		printf("write to [%p]\n", pg_round_down(fault_addr));
+		printf("present\n");
+		printf("write %d\n", write);
+		printf("user %d\n", user);
+		printf("write to [%p]\n", pg_round_down(fault_addr));
 		if (user)
 			kill(f);
 		else {
