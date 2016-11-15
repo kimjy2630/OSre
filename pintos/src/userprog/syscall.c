5c552006e1ffac0b4165db3cb59dc090aa2e9016
Jaeyoon Kim <Jaeyoon@143.248.180.79>
2016-11-15 12:09:22 +0900

    asdf

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -273,6 +273,7 @@ int write(int fd, const void *buffer, unsigned length) {
 	unsigned rest = length;
 	void *tmp_buf = (void *) buffer;
 	int cnt = 0;
+	printf("write rest %d tmp_buf %p\n", rest, tmp_buf);
 	while (rest > 0) {
 		printf("write rest %d tmp_buf %p\n", rest, tmp_buf);
 		size_t ofs = tmp_buf - pg_round_down(tmp_buf);
