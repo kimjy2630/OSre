671989d1da29aa1f692fd1664c79e3ba826ab5e2
하진철 <smilecjf@naver.com>
2016-11-15 20:13:51 +0900

    sfda

--- pintos/src/lib/kernel/hash.c
+++ pintos/src/lib/kernel/hash.c
@@ -131,7 +131,6 @@ hash_replace (struct hash *h, struct hash_elem *new)
 struct hash_elem *
 hash_find (struct hash *h, struct hash_elem *e) 
 {
-	printf("hash_find: start find_elem\n");
   return find_elem (h, find_bucket (h, e), e);
 }
 
