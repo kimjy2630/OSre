8fb8c9553dc0e8a88ddff609073bd8bd36d24cd3
하진철 <smilecjf@naver.com>
2016-11-15 15:27:55 +0900

    asdf

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -492,13 +492,13 @@ static bool load_segment(struct file *file, off_t ofs, uint8_t *upage, uint32_t 
 
 		/* Get a page of memory. */
 #ifdef VM
-		struct supp_page_entry *spe = supp_page_add(upage, writable);
-//		printf("load_segment spe uaddr:%p\n", upage);
-		if(upage >= PHYS_BASE){
+		if(upage >= PHYS_BASE) {
 			printf("upage:%p\n", upage);
 			printf("kernel access!\n");
 			exit(-1);
 		}
+		struct supp_page_entry *spe = supp_page_add(upage, writable);
+//		printf("load_segment spe uaddr:%p\n", upage);
 		if(page_zero_bytes == PGSIZE)
 			spe->type = ZERO;
 		else
