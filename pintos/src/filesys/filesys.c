e33b5db23131c8a5149346c22efa2aba9d92a638
하진철 <smilecjf@naver.com>
2016-11-15 19:44:34 +0900

    asfdaf

--- pintos/src/filesys/filesys.c
+++ pintos/src/filesys/filesys.c
@@ -70,6 +70,8 @@ filesys_open (const char *name)
   struct dir *dir = dir_open_root ();
   struct inode *inode = NULL;
 
+  if(dir == NULL)
+	  printf("filesys_open dir NULL\n");
   if (dir != NULL)
     dir_lookup (dir, name, &inode);
   dir_close (dir);
