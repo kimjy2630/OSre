31f579a27fa9276c842b0725df623e719ade9761
하진철 <smilecjf@naver.com>
2016-11-19 04:41:02 +0900

    lock_page

--- pintos/src/vm/page.c
+++ pintos/src/vm/page.c
@@ -96,7 +96,9 @@ void supp_page_entry_destroy(struct hash_elem *e, void *aux) {
 void supp_page_table_destroy(struct hash *supp_page_table) {
 	//TODO
 	frame_releaes_lock_frame();
+	lock_acquire(&lock_page);
 	hash_destroy(supp_page_table, supp_page_entry_destroy);
+	lock_release(&lock_page);
 }
 
 struct supp_page_entry* stack_grow(void* addr) {
