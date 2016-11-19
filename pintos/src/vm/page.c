46f724540a547bb1c9fe1a56a46d36dd54e0186c
하진철 <smilecjf@naver.com>
2016-11-19 11:57:11 +0900

    page.c hash destr swap free

--- pintos/src/vm/page.c
+++ pintos/src/vm/page.c
@@ -96,9 +96,9 @@ void supp_page_entry_destroy(struct hash_elem *e, void *aux) {
 //		free(fe);
 		frame_free(spe);
 	}
-//	if(spe->type == SWAP && spe->swap_index != NULL) {
-//		swap_free(spe->swap_index);
-//	}
+	if(spe->type == SWAP && spe->swap_index != NULL) {
+		swap_free(spe->swap_index);
+	}
 	lock_release(&spe->lock);
 	free(spe);
 }
