66c13ead1975f051c1b4c9a55eed63f151fb79d4
하진철 <smilecjf@naver.com>
2016-11-19 21:51:42 +0900

    frame.h frame add

--- pintos/src/vm/frame.h
+++ pintos/src/vm/frame.h
@@ -16,7 +16,7 @@ struct frame_entry {
 
 void frame_init();
 //struct frame_entry* frame_lookup(uint8_t *kddr);
-//struct frame_entry* frame_add(enum palloc_flags flags);
+struct frame_entry* frame_add(enum palloc_flags flags);
 //void frame_free(struct frame_entry *fe);
 //void frame_free(void* addr);
 
