7ebabdf9a11bc926cb0424522e8e8a752b081ef8
하진철 <smilecjf@naver.com>
2016-11-19 20:30:23 +0900

    afd

--- pintos/src/vm/swap.c
+++ pintos/src/vm/swap.c
@@ -33,11 +33,11 @@ size_t swap_load(uint8_t *addr){ // mem -> disk
 		PANIC("swap disk full");
 	}
 	int i;
-	lock_acquire(&swap_lock);
 	for (i = 0; i < num_sector_in_page; ++i) {
+		lock_acquire(&swap_lock);
 		disk_write(swap_disk, index + i, addr + i * DISK_SECTOR_SIZE);
+		lock_release(&swap_lock);
 	}
-	lock_release(&swap_lock);
 //	printf("load end\n");
 	return index;
 }
@@ -53,12 +53,12 @@ void swap_unload(size_t index, uint8_t *addr) { // disk -> mem
 //	printf("bit set mul\n");
 
 	int i;
-	lock_acquire(&swap_lock);
 	for (i = 0; i < num_sector_in_page; ++i) {
 //		printf("access %p\n", uaddr + i * DISK_SECTOR_SIZE);
+		lock_acquire(&swap_lock);
 		disk_read(swap_disk, index + i, addr + i * DISK_SECTOR_SIZE);
+		lock_release(&swap_lock);
 	}
-	lock_release(&swap_lock);
 //	printf("disk_read\n");
 //	printf("unload end\n");
 }
