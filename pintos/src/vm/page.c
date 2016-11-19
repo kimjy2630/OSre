a6c072f445054dc9473756910c25c4b2f24a997b
하진철 <smilecjf@naver.com>
2016-11-19 21:36:20 +0900

    sfda

--- pintos/src/vm/page.c
+++ pintos/src/vm/page.c
@@ -32,7 +32,7 @@ struct supp_page_entry* supp_page_add(uint8_t *addr, bool writable) {
 //	lock_init(&spe->lock);
 
 //	lock_acquire(&spe->t->lock_page);
-	printf("lock_page name %s tid %d\n", thread_current()->name, thread_current()->tid);
+	printf("lock_page\n");
 	lock_acquire(&lock_page);
 	hash_insert(&supp_page_table, &spe->elem);
 	lock_release(&lock_page);
