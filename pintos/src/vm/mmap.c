e39a9c44125222d08136f516c99200d3afecc111
하진철 <smilecjf@naver.com>
2016-11-24 16:46:55 +0900

    sadf

--- pintos/src/vm/mmap.c
+++ pintos/src/vm/mmap.c
@@ -5,7 +5,7 @@
 unsigned hash_mapid(struct hash_elem *e, void *aux) {
 	struct mmapping *mmapping;
 	mmapping = hash_entry(e, struct mmapping, elem);
-	return hash_int(mmapping->mapid);
+	return hash_bytes(&mmapping->mapid, sizeof(mmapping->mapid));
 }
 
 bool hash_less_mapid(const struct hash_elem *a, const struct hash_elem *b, void *aux) {
