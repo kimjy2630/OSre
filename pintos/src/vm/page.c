7082fc271975f1120cd8e13e4bcb7e348d4a5c37
Jaeyoon Kim <Jaeyoon@143.248.180.79>
2016-11-25 23:52:35 +0900

    cancel roll back

--- pintos/src/vm/page.c
+++ pintos/src/vm/page.c
@@ -113,7 +113,7 @@ void supp_page_entry_destroy(struct hash_elem *e, void *aux) {
 		lock_acquire(&lock_file);
 		file_write_at(file, kaddr, spe->mmap_page_read_bytes, spe->mmap_ofs);
 		lock_release(&lock_file);
-//		pagedir_clear_page(spe->t->pagedir, spe->uaddr);
+		pagedir_clear_page(spe->t->pagedir, spe->uaddr);
 		frame_free_fe(fe);
 	} else if(spe->type == SWAP && spe->swap_index != NULL) {
 		swap_free(spe->swap_index);
