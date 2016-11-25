1eb30109541f74ab8dce21eb7097934e60f54668
하진철 <smilecjf@naver.com>
2016-11-25 18:05:27 +0900

    debug mmap-unmap x

--- pintos/src/tests/vm/mmap-unmap.c
+++ pintos/src/tests/vm/mmap-unmap.c
@@ -15,9 +15,8 @@ test_main (void)
   mapid_t map;
 
   CHECK ((handle = open ("sample.txt")) > 1, "open \"sample.txt\"");
-//  map = mmap (handle, ACTUAL);
   CHECK ((map = mmap (handle, ACTUAL)) != MAP_FAILED, "mmap \"sample.txt\"");
-  printf("map %d\n", map);
+
   munmap (map);
 
   fail ("unmapped memory is readable (%d)", *(int *) ACTUAL);
