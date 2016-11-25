77751ab4aec84504fa615a76fe52c9b13ca2050b
하진철 <smilecjf@naver.com>
2016-11-25 21:33:55 +0900

    debug pagefalut printf MMAP

--- pintos/src/userprog/exception.c
+++ pintos/src/userprog/exception.c
@@ -251,10 +251,9 @@ static void page_fault(struct intr_frame *f) {
 //				pagedir_set_dirty (t->pagedir, uaddr, true);
 //				printf("swap sfad\n");
 			} else if(spe->type == MMAP){
+				printf("pagefault MMAP\n");
 				struct file *file = spe->mmap->file;
 				ASSERT(file != NULL);
-//				off_t length = file_length(file);
-//				printf("pagefault: add MEM_MMAP page, file %p, length %u\n", file, length);
 //				file_seek(file, spe->mmap_ofs);
 				off_t bytes_read = file_read_at(file, kaddr, spe->mmap_page_read_bytes, spe->mmap_ofs);
 //				memset(kaddr + bytes_read, 0, PGSIZE - bytes_read);
