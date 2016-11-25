549eab894b6c8613a4f6f0b0eaffded5d2c99bf0
하진철 <smilecjf@naver.com>
2016-11-25 18:46:10 +0900

    debug pagefault MMAP, mmap-close

--- pintos/src/userprog/exception.c
+++ pintos/src/userprog/exception.c
@@ -251,6 +251,7 @@ static void page_fault(struct intr_frame *f) {
 //				pagedir_set_dirty (t->pagedir, uaddr, true);
 //				printf("swap sfad\n");
 			} else if(spe->type == MMAP){
+				printf("pagefault: add MEM_MMAP page\n");
 				struct file *file = spe->mmap->file;
 				ASSERT(file != NULL);
 //				file_seek(file, spe->mmap_ofs);
