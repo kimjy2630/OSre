93fcc9fcf46054845374389568f1a862eab47323
하진철 <smilecjf@naver.com>
2016-11-25 20:54:27 +0900

    roll back mmap-misalign, mmap_cnt init 0

--- pintos/src/tests/vm/mmap-misalign.c
+++ pintos/src/tests/vm/mmap-misalign.c
@@ -11,8 +11,7 @@ test_main (void)
   
   CHECK ((handle = open ("sample.txt")) > 1, "open \"sample.txt\"");
   mapid_t map = mmap (handle, (void *) 0x10001234);
-  printf("map %d\n", map);
-//  CHECK (mmap (handle, (void *) 0x10001234) == MAP_FAILED,
-//         "try to mmap at misaligned address");
+  CHECK (mmap (handle, (void *) 0x10001234) == MAP_FAILED,
+         "try to mmap at misaligned address");
 }
 
