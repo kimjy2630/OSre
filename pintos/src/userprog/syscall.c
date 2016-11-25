bbc48f40b2f7520b2e0714deef08b310100a59b8
하진철 <smilecjf@naver.com>
2016-11-26 04:05:58 +0900

    sadf

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -484,7 +484,8 @@ void munmap(mapid_t mapid){
 //			struct file *file = spe->mmap->file;
 			struct file *file = spe->mmap->file;
 			lock_acquire(&lock_file);
-			file_write_at(file, kaddr, spe->mmap_page_read_bytes, spe->mmap_ofs);
+			file_seek(file, spe->mmap_ofs);
+			file_write(file, kaddr, spe->mmap_page_read_bytes);
 			lock_release(&lock_file);
 			pagedir_clear_page(spe->t->pagedir, spe->uaddr);
 			frame_free_fe(spe->fe);
