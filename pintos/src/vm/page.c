1152996063980d2d897d10404b08fe5ec0f21fa4
하진철 <smilecjf@naver.com>
2016-11-26 17:01:54 +0900

    page_supp_table_destr MEM_MMAP x

--- pintos/src/vm/page.c
+++ pintos/src/vm/page.c
@@ -110,13 +110,13 @@ void supp_page_entry_destroy(struct hash_elem *e, void *aux) {
 //		frame_free_fe(fe);
 //	} else
 	if (fe != NULL) {
-		if (spe->type == MEM_MMAP) {
-			if (pagedir_is_dirty(spe->t->pagedir, spe->uaddr)) {
-				struct file *file = spe->mmap->file;
-				lock_acquire(&lock_file);
-				file_write_at(file, spe->kaddr, spe->mmap_page_read_bytes, spe->mmap_ofs);
-				lock_release(&lock_file);
-			}
+//		if (spe->type == MEM_MMAP) {
+//			if (pagedir_is_dirty(spe->t->pagedir, spe->uaddr)) {
+//				struct file *file = spe->mmap->file;
+//				lock_acquire(&lock_file);
+//				file_write_at(file, spe->kaddr, spe->mmap_page_read_bytes, spe->mmap_ofs);
+//				lock_release(&lock_file);
+//			}
 		}
 		pagedir_clear_page(spe->t->pagedir, spe->uaddr);
 		//TODO
