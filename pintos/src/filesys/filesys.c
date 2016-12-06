d3454c041644b46c9eb17aa963135710933e9174
하진철 <smilecjf@naver.com>
2016-12-07 06:06:53 +0900

    afd

--- pintos/src/filesys/filesys.c
+++ pintos/src/filesys/filesys.c
@@ -49,17 +49,18 @@ filesys_create (const char *name, off_t initial_size)
 {
   disk_sector_t inode_sector = 0;
   struct dir *dir = dir_open_root ();
+  bool inode_create_success = inode_create(inode_sector, initial_size);
   bool success = (dir != NULL
                   && free_map_allocate (1, &inode_sector)
-                  && inode_create (inode_sector, initial_size)
+                  && inode_create_success // inode_create (inode_sector, initial_size)
                   && dir_add (dir, name, inode_sector));
   if (!success && inode_sector != 0) 
     free_map_release (inode_sector, 1);
   dir_close (dir);
 
   // debug
-  if(!success){
-	  printf("filesys_create fail\n");
+  if(!inode_create_success){
+	  printf("filesys_create: inode_create fail\n");
   }
 
   return success;
