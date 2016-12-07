f43bccc4fc3787543f1d22f7a3a71b5521c74e74
하진철 <smilecjf@naver.com>
2016-12-08 01:34:58 +0900

    asdf

--- pintos/src/filesys/inode.c
+++ pintos/src/filesys/inode.c
@@ -252,7 +252,8 @@ bool grow_inode(struct inode_disk *disk_inode, off_t length){
 			disk_read(filesys_disk, disk_inode->list_sector[124], indirect);
 		}
 
-		for(i = curr_num_sector; i < (num_sector - DIRECT) && i < SINGLE_INDIRECT; i++){
+//		for(i = curr_num_sector; i < (num_sector - DIRECT) && i < 128; i++){
+		for(i = 0; i < (num_sector - DIRECT) && i < 128; i++){
 			printf("grow_inode: print i = %zu\n", i);
 			if(free_map_allocate(1, &direct_sector)){
 				disk_write(filesys_disk, direct_sector, zeros);
