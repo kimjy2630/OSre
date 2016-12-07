dfee9bf708087a5753dc9019aeb61cbb1f6fd78e
하진철 <smilecjf@naver.com>
2016-12-08 04:32:41 +0900

    cache.c read/write merge

--- pintos/src/filesys/cache.c
+++ pintos/src/filesys/cache.c
@@ -10,7 +10,7 @@ int cache_num;
 struct lock lock_cache;
 
 #define CACHE_MAX 64
-
+struct cache_entry *cache(disk_sector_t sector_idx, bool dirty);
 
 void cache_init(){
 	list_init(&list_cache);
@@ -34,6 +34,55 @@ struct cache_entry *cache_find(disk_sector_t sector_idx){
 	return NULL;
 }
 
+struct cache_entry *cache(disk_sector_t sector_idx, bool dirty){
+	struct cache_entry *ce = cache_find(sector_idx);
+	if(ce != NULL){
+		ce->access = true;
+		if(dirty)
+			ce->dirty = true;
+		return ce;
+	}
+
+	ce = malloc(sizeof(struct cache_entry));
+	if(ce == NULL){
+		printf("cache_read: not enough space to make a cache_entry\n");
+		return NULL;
+	}
+	ce->sector_idx = sector_idx;
+	ce->dirty = dirty;
+	ce->access = true;
+
+	uint8_t *sector = NULL;
+	sector = malloc(DISK_SECTOR_SIZE);
+	if(sector == NULL){
+		printf("cache_read: not enough space to make a cache_sector\n");
+		free(ce);
+		return NULL;
+	}
+	disk_read(filesys_disk, sector_idx, sector);
+	ce->sector = sector;
+
+	lock_acquire(&lock_cache);
+	if(cache_num < CACHE_MAX){
+		list_push_back(&list_cache, &ce->elem);
+		cache_num++;
+	} else{
+		cache_evict();
+		list_push_back(&list_cache, &ce->elem);
+	}
+	lock_release(&lock_cache);
+	return ce;
+}
+
+struct cache_entry *cache_read(disk_sector_t sector_idx){
+	return cache(sector_idx, false);
+}
+
+struct cahche_entry *cache_write(disk_sector_t sector_idx){
+	return cache(sector_idx, true);
+}
+
+/*
 struct cache_entry *cache_read(disk_sector_t sector_idx){
 	struct cache_entry *ce = cache_find(sector_idx);
 	if(ce != NULL){
@@ -71,7 +120,8 @@ struct cache_entry *cache_read(disk_sector_t sector_idx){
 	lock_release(&lock_cache);
 	return ce;
 }
-
+*/
+/*
 struct cache_entry *cache_write(disk_sector_t sector_idx){
 	struct cache_entry *ce = cache_find(sector_idx);
 	if(ce != NULL){
@@ -110,6 +160,7 @@ struct cache_entry *cache_write(disk_sector_t sector_idx){
 	lock_release(&lock_cache);
 	return ce;
 }
+*/
 
 void cache_evict(){
 	struct list_elem *e;
