e4bcdf2f0bcabb5b29a430ca81cdcf3e8cfc64bb
하진철 <smilecjf@naver.com>
2016-12-09 02:27:02 +0900

    file_write check inode is_dir

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -318,10 +318,10 @@ int write(int fd, const void *buffer, unsigned length) {
 		return 0;
 	}
 
-#ifdef FILESYS
-	if (inode_is_dir(file_get_inode(pf->file)))
-		return -1;
-#endif
+//#ifdef FILESYS
+//	if (inode_is_dir(file_get_inode(pf->file)))
+//		return -1;
+//#endif
 	/*
 	 size_t cnt = 0;
 
