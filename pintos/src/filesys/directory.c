ec9a3e640d4a5f902912d70dcb3eab9d028fd109
하진철 <smilecjf@naver.com>
2016-12-09 05:17:41 +0900

    debug dir_add printf

--- pintos/src/filesys/directory.c
+++ pintos/src/filesys/directory.c
@@ -278,6 +278,7 @@ dir_add (struct dir *dir, const char *name, disk_sector_t inode_sector, bool is_
   e.in_use = true;
   strlcpy (e.name, name, sizeof e.name);
   e.inode_sector = inode_sector;
+  printf("dir_add: dir in use, name [%s], inode_sector [%d]\n", name, inode_sector);
   success = inode_write_at (dir->inode, &e, sizeof e, ofs) == sizeof e;
 
  done:
