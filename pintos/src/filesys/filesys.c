b88ac1baa1c2b0f79ec279342f0666ad51ab08fa
하진철 <smilecjf@naver.com>
2016-12-09 04:10:18 +0900

    sdaf

--- pintos/src/filesys/filesys.c
+++ pintos/src/filesys/filesys.c
@@ -94,7 +94,7 @@ filesys_open(const char *name) {
 	memset(filename, 0, length);
 //	printf("filesys_open: before parse, path [%s], filename [%s]\n", path, filename);
 	parse_dir(name, path, filename);
-//	printf("filesys_open: name [%s], path [%s], filename [%s]\n", name, path, filename);
+	printf("filesys_open: name [%s], path [%s], filename [%s]\n", name, path, filename);
 	struct dir *dir = dir_open_path(path);
 	struct inode *inode = NULL;
 
