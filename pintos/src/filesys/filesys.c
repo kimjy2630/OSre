5f883f3890b3dc0abbf30beb6433fb283c496c98
하진철 <smilecjf@naver.com>
2016-12-09 01:58:54 +0900

    filesys_create parsed name

--- pintos/src/filesys/filesys.c
+++ pintos/src/filesys/filesys.c
@@ -63,8 +63,8 @@ filesys_create (const char *name, off_t initial_size, bool is_dir)
   struct dir *dir = dir_open_path(path);
   bool success = (dir != NULL
                   && free_map_allocate (1, &inode_sector)
-                  && inode_create (inode_sector, initial_size, is_dir)
-                  && dir_add (dir, name, inode_sector, is_dir));
+                  && inode_create (inode_sector, initial_size, is_dir) //
+                  && dir_add (dir, filename, inode_sector, is_dir)); //
   if (!success && inode_sector != 0) 
     free_map_release (inode_sector, 1);
   dir_close (dir);
