143b3d76cb5d9586aa647f123d9e95e6bda0969b
하진철 <smilecjf@naver.com>
2016-12-09 00:34:52 +0900

    asfd

--- pintos/src/filesys/filesys.c
+++ pintos/src/filesys/filesys.c
@@ -75,7 +75,7 @@ filesys_open(const char *name) {
 	char path[length+1];
 	char filename[length+1];
 	parse_dir(name, path, filename);
-	printf("filesys_open: name %s, path %s, filename %s\n", name, path, filename);
+	printf("filesys_open: name [%s], path [%s], filename [%s]\n", name, path, filename);
 	struct dir *dir = dir_open_path(path);
 	struct inode *inode = NULL;
 
