27b4b9fccb89cc08faf3c9177eefd9e167077648
하진철 <smilecjf@naver.com>
2016-12-09 11:35:26 +0900

    asfd

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -165,7 +165,7 @@ bool remove(const char *file) {
 	}
 	struct file* f = filesys_open(file);
 	if (f == NULL){
-		printf("remove: filesys_open(file) is NULL. file [%s]\n", file);
+//		printf("remove: filesys_open(file) is NULL. file [%s]\n", file);
 		return false;
 	}
 	file_close(f);
@@ -557,7 +557,7 @@ bool chdir(const char* dir) {
 	if(curr_dir != NULL)
 		dir_close(curr_dir);
 	thread_current()->curr_dir = dir_target;
-	printf("chdir: change dir to [%s]\n", dir);
+//	printf("chdir: change dir to [%s]\n", dir);
 	return true;
 }
 bool mkdir(const char* dir) {
@@ -580,10 +580,10 @@ bool readdir(int fd, const char* name) {
 	struct dir *dir = pf->dir;
 	ASSERT(dir != NULL);
 	bool success = dir_readdir(dir, name);
-	if(success)
-		printf("readdir: name [%s]\n", name);
-	else
-		printf("readdir: fail\n");
+//	if(success)
+//		printf("readdir: name [%s]\n", name);
+//	else
+//		printf("readdir: fail\n");
 	return success;
 }
 bool isdir(int fd) {
