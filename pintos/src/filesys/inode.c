3b15f5de67374e76ca9b76b97f70bc2577a69413
하진철 <smilecjf@naver.com>
2016-12-10 04:08:31 +0900

    asdf

--- pintos/src/filesys/inode.c
+++ pintos/src/filesys/inode.c
@@ -88,7 +88,7 @@ byte_to_sector (const struct inode *inode, off_t pos)
 //			/*
 			indirect = malloc(sizeof(struct indirect_sector));
 			if(inode->data.list_sector[123] == -1){
-				printf("inode length %u, sector %d\n", inode->data.length, sector);
+				printf("inode length %u, pos %u, sector %d\n", inode->data.length, pos, sector);
 				int i;
 				for(i=0; i<125; i++){
 					printf("inode->data.list_sector[%d] : %d\n", i, inode->data.list_sector[i]);
