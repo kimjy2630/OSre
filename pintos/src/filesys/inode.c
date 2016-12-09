694f8981a80ee5e44cf01620ad11d442b35746a2
하진철 <smilecjf@naver.com>
2016-12-10 04:02:28 +0900

    sadf

--- pintos/src/filesys/inode.c
+++ pintos/src/filesys/inode.c
@@ -310,7 +310,7 @@ bool grow_inode(struct inode_disk *disk_inode, off_t length){
 			*/
 		}
 
-		for(i = curr_num_sector - DIRECT; i < (num_sector - DIRECT) && i < 128; i++){
+		for(i = curr_num_sector - DIRECT; i < (num_sector - DIRECT) + 1 && i < 128; i++){
 //			printf("grow_inode: print i = %zu\n", i);
 			if(free_map_allocate(1, &direct_sector)){
 //				/*
