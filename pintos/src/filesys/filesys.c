b2dccd6ec17bcf7d508eb5326c48d91e584a2f33
하진철 <smilecjf@naver.com>
2016-12-10 19:47:16 +0900

    asdf

--- pintos/src/filesys/filesys.c
+++ pintos/src/filesys/filesys.c
@@ -7,6 +7,7 @@
 #include "filesys/inode.h"
 #include "filesys/directory.h"
 #include "devices/disk.h"
+#include "threads/synch.h"
 
 /* The disk that contains the file system. */
 struct disk *filesys_disk;
